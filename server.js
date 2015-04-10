d32fc000928bf979a0f63025e886d00e40ca2542
Montana Flynn <montana@montanaflynn.me>
2015-04-10 03:59:39 -0700

    Remove CORS, add marco-polo endpoint, and check for missing target url

--- server.js
+++ server.js
@@ -3,15 +3,24 @@ var apistatus = require('apistatus')
 
 var server = restify.createServer()
 
-server.use(restify.CORS())
 server.use(restify.queryParser())
 
 server.get('/', function (req, res, next) {
   var url = req.params.url
+  if (!url) {
+    res.send({error: "missing target url"})
+    next()
+    return
+  }
   apistatus(url, function(status){
     res.send(status)
     next()
   })
+})
+
+server.get('/marco', function (req, res, next) {
+  res.send("polo")
+  next()
 })
 
 server.listen(8080, function() {
